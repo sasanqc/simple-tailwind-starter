39e8920edfa91c885bc3c0a704c32fe142ba5736
sasanqc <sasanqc@gmail.com>
2023-06-08 12:57:09 +0330

    updated breakpoints in confi file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
   theme: {
     extend: {},
   },
